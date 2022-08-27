8e743e87cbaaeb46d273badd3341720838f0a980
Huguito3 <hduraniroz@gmail.com>
2022-08-27 13:51:51 -0300

    alteracao

--- providers.tf
+++ providers.tf
@@ -10,7 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/hugo_iroz/gcpkey.json")
 
   project = "cedar-scene-360015"
   region  = "us-west1"
